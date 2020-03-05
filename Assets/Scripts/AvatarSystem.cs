f7eca6832f7eb7ab70559bcf1be92ba11e7abcc2
RECNEPS-KIPS <2432719656@qq.com>
2020-03-05 20:12:04 +0800

    实现了存储模型的数据信息
    基于嵌套字典

--- Assets/Scripts/AvatarSystem.cs
+++ Assets/Scripts/AvatarSystem.cs
@@ -10,17 +10,23 @@ public class AvatarSystem : MonoBehaviour {
 
     private Transform[] girlHips;//girl骨骼信息
     private Transform gsTrans;
+    Dictionary<string, SkinnedMeshRenderer> girlSMR = new Dictionary<string, SkinnedMeshRenderer>();//换装骨骼上的MeshReneder信息
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         InstantiateSource();
         InstantiateTarget();
+        girlHips = girlTarget.GetComponentsInChildren<Transform>();
+        DataSave();
+        //foreach (KeyValuePair<string,Dictionary<string,SkinnedMeshRenderer>> gd in girlData) {
+        //    foreach (var smr in gd.Value) {
+        //        Debug.Log(gd.Key+" "+smr.Key+" "+smr.Value);
+        //    }
+        //}
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 
     void InstantiateSource() {
@@ -31,6 +37,28 @@ public class AvatarSystem : MonoBehaviour {
 
     void InstantiateTarget() {
         girlTarget = Instantiate(Resources.Load<GameObject>("FemaleTarget"));
+    }
 
+    void DataSave() {
+        if (gsTrans == null) {
+            return;
+        }
+        //遍历所有子物体的SkinnedMeshRenderer组件添加到数组中存储
+        SkinnedMeshRenderer[] parts = gsTrans.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var part in parts) {
+            string[] names = part.name.Split('-');
+            if (!girlData.ContainsKey(names[0])) {
+                //生成对应部位 且只生成一个
+                GameObject partGo = new GameObject {
+                    name = names[0]
+                };
+                partGo.transform.parent = girlTarget.transform;
+                //把骨骼target身上的SkinnedMeshReneder信息存储
+                girlSMR.Add(names[0],partGo.AddComponent<SkinnedMeshRenderer>());
+
+                girlData.Add(names[0],new Dictionary<string, SkinnedMeshRenderer>());
+            }
+            girlData[names[0]].Add(names[1],part);//存储所有的SkinnedMeshReneder信息
+        }
     }
 }
