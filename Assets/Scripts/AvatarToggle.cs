1863f75264118cb984e35701a7f63751202c7796
RECNEPS-KIPS <2432719656@qq.com>
2020-03-07 18:17:50 +0800

    实现了换装动画的播放

--- Assets/Scripts/AvatarToggle.cs
+++ Assets/Scripts/AvatarToggle.cs
@@ -29,7 +29,28 @@ public class AvatarToggle : MonoBehaviour {
             }
             string[] names = this.name.Split('-');//name数组索引0为部位,索引1为部位序号
             AvatarSystem.instance.ClothesReplace(names[0],names[1]);
+            switch (names[0]) {
+                case "pants":
+                    AnimationPlay("item_pants");
+                    break;
+                case "shoes":
+                    AnimationPlay("item_boots");
+                    break;
+                case "top":
+                    AnimationPlay("item_shirt");
+                    break;
+                default:
+                    break;
+            }
         }
-        
+    }
+
+    public void AnimationPlay(string animName) {
+        //Debug.Log(animName);
+        Animation anim = GameObject.FindGameObjectWithTag("Model").GetComponent<Animation>();
+        if (!anim.IsPlaying(animName)) {
+            anim.Play(animName);
+            anim.PlayQueued("idle1");
+        }
     }
 }
