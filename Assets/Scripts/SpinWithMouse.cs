840f619e8da022b438ac91ed5d71ec5e866b2a74
RECNEPS-KIPS <2432719656@qq.com>
2020-03-07 18:39:32 +0800

    实现了模型的拖拽旋转

--- Assets/Scripts/SpinWithMouse.cs
+++ Assets/Scripts/SpinWithMouse.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinWithMouse : MonoBehaviour
-{
+public class SpinWithMouse : MonoBehaviour {
+    private bool isDown = false;//鼠标是否按下
+
+    public Vector3 curPos;
+    public Vector3 lastPos;
+
+    private float length = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +16,21 @@ public class SpinWithMouse : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        curPos = Input.mousePosition;
+        if (isDown) {
+            Vector3 offset = curPos - lastPos;
+            if (Mathf.Abs(offset.x)> Mathf.Abs(offset.y)&&Mathf.Abs(offset.x)>length) {
+                transform.Rotate(Vector3.up,-offset.x);
+            }
+        }
+        lastPos= Input.mousePosition;
+    }
+
+    void OnMouseUp() {
+        isDown = false;
+    }
+    void OnMouseDown() {
+        isDown = true;
     }
 }
