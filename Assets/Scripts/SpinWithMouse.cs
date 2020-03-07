8f3e575c434a870a8bf7bddc42a7c5f5f5e9269c
RECNEPS-KIPS <2432719656@qq.com>
2020-03-07 18:42:38 +0800

    添加解析

--- Assets/Scripts/SpinWithMouse.cs
+++ Assets/Scripts/SpinWithMouse.cs
@@ -8,7 +8,7 @@ public class SpinWithMouse : MonoBehaviour {
     public Vector3 curPos;
     public Vector3 lastPos;
 
-    private float length = 5;
+    private float length = 5;//鼠标最小位移
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +17,14 @@ public class SpinWithMouse : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        curPos = Input.mousePosition;
+        curPos = Input.mousePosition;//当前帧鼠标位置
         if (isDown) {
             Vector3 offset = curPos - lastPos;
             if (Mathf.Abs(offset.x)> Mathf.Abs(offset.y)&&Mathf.Abs(offset.x)>length) {
                 transform.Rotate(Vector3.up,-offset.x);
             }
         }
-        lastPos= Input.mousePosition;
+        lastPos= Input.mousePosition;//上一帧鼠标位置
     }
 
     void OnMouseUp() {
