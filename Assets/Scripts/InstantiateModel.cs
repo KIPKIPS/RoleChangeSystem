181f7da24fa277699be4a02e88b8832e4f1efcc0
RECNEPS-KIPS <2432719656@qq.com>
2020-03-08 12:14:28 +0800

    修复了新场景错误的鼠标交互操作

---
+++ Assets/Scripts/InstantiateModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        if (AvatarSystem.instance.sex==0) {
+            AvatarSystem.instance.Girl();
+        }
+        else {
+            AvatarSystem.instance.Boy();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
